2b40af1be6f5fc4e612b3aa90843b009f6d8f6ce
David <dr507498@gmail.com>
2025-04-18 11:17:58 -0400

    edits

--- app.py
+++ app.py
@@ -252,15 +252,3 @@ def smartspending():
         ten_points = invoke_llm(data=session['all_transactions'], llm=llm)
         
     return render_template("smartspending.html", ten_points=ten_points)
-
-# Error handlers
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-if __name__ == "__main__":
-    app.run(debug=os.getenv("FLASK_DEBUG", "False") == "True")
